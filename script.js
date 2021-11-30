a7b9a9312b105ced57aacc60974a5605622542a4
ZaidAbsyar <zaidabsyar43@gmail.com>
2021-11-30 08:16:32 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function func(num) {
+    //let umur = prompt('berapa umur anda?');
+   document.getElementById('tajuk').innerText =  func2(num);
+     
+   // return umur;
+}
+
+function func2(num) {
+    return 'Anda berumur ' + num;
+}
